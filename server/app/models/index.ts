4be5eaa8e056d812db72ad1c553d7a47546ad7bd
Karin <karin.stenwall@gmail.com>
2021-09-23 17:32:50 +0200

    feat: add models for 'user' and 'role' #26

--- server/app/models/index.ts
+++ server/app/models/index.ts
@@ -1,14 +1,14 @@
-import config from '../../config';
 import mongoose from 'mongoose';
+import Role from './role.model';
+import User from './user.model';
 
 mongoose.Promise = global.Promise;
 
 const db = {
   mongoose: mongoose,
-  uri: config.DB_URI as string,
-  dbName: process.env.DB_NAME as string,
-  user: process.env.DB_USER as string,
-  pass: process.env.DB_PASS as string,
+  user: User,
+  role: Role,
+  ROLES: ['user', 'admin']
 };
 
 export default db;
--- server/server.ts
+++ server/server.ts
@@ -9,14 +9,16 @@ const app = express();
 dotenv.config();
 
 const port = parseInt(config.PORT as string, 10);
+const mongoURI = config.DB_URI as string;
+const Role = db.role;
 
 const corsOptions = {
   origin: config.CLIENT_URL as string
 };
 const mongooseOptions = {
-  dbName: db.dbName,
-  user: db.user,
-  pass: db.pass,
+  dbName: process.env.DB_NAME as string,
+  user: process.env.DB_USER as string,
+  pass: process.env.DB_PASS as string,
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
@@ -28,14 +30,43 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 db.mongoose
-  .connect(db.uri, mongooseOptions)
+  .connect(mongoURI, mongooseOptions)
   .then(() => {
-    console.log('Connected to the database!');
+    console.log('Connected to the database.');
+    initial();
   })
   .catch(err => {
-    console.log('Cannot connect to the database!', err);
+    console.log('Cannot connect to the database.', err);
     process.exit();
   });
+
+function initial() {
+  Role.estimatedDocumentCount((err: any, count: number) => {
+    if (!err && count === 0) {
+      new Role({
+        name: 'user'
+      })
+      .save(err => {
+        if (err) {
+          console.log('error', err);
+        }
+
+        console.log('added `user` to roles collection');
+      });
+
+      new Role({
+        name: 'admin'
+      })
+      .save(err => {
+        if (err) {
+          console.log('error', err);
+        }
+
+        console.log('added `admin` to roles collection');
+      });
+    }
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}.`);
