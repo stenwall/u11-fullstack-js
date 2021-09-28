826d17e9ad649511e2c2a4414f9255069a038bf3
Karin <karin.stenwall@gmail.com>
2021-09-28 20:58:25 +0200

    feat: model, controller, routes for posts #15

---
+++ server/app/controllers/post.controller.ts
@@ -0,0 +1,136 @@
+// controller for posts
+import { Request, Response } from 'express';
+import { dbModel, PostDocument } from '../models';
+
+const Post = dbModel.Post;
+
+// create and save a new post
+export const createPost = async (req: Request, res: Response) => {
+  if (!req.body) {
+    res.status(400).send({ message: 'Cannot be empty.' });
+    return;
+  }
+
+  try {
+    const post = await Post.create(req.body);
+    return res.status(201).send({
+      message: 'Post created successfully.',
+      post,
+    });
+  } catch (err: any) {
+    return res.status(400).send({
+      message: 'Error saving post to database.',
+      error: err.message
+    });
+  }
+};
+
+// find post by id
+export const findOnePost = async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  try {
+    const post = await Post.findById(id);
+    if (!post) {
+      return res.status(404).send({ message: `Post with id ${id} not found.` });
+    } else {
+      return res.status(200).send(post);
+    }
+  } catch (err: any) {
+    return res.status(500).send({
+      message: `Error retrieving post with id ${id}.`,
+      error: err.message
+    });
+  }
+};
+
+// get post populated with comments
+export const getPostWithComments = async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  try {
+    const post = await Post.findById(id).populate('comments', '-__v');
+    if (!post) {
+      return res.status(404).send({ message: `Post with id ${id} not found.` });
+    } else {
+      return res.status(200).send(post);
+    }
+  } catch (err: any) {
+    return res.status(500).send({
+      message: `Error retrieving post with id ${id}.`,
+      error: err.message
+    });
+  }
+}
+
+// find all posts by author
+export const findPostsByUserId = async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  try {
+    const posts = await Post.find({ user_id: id }).exec();
+    return res.status(201).send({
+      posts
+    });
+  } catch (err: any) {
+    return res.status(400).send({
+      message: 'Error saving post to database.',
+      error: err.message
+    });
+  }
+};
+
+// update post by id
+export const updatePost = async (req: Request, res: Response) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: 'Data to update cannot be empty.'
+    });
+  }
+
+  const id = req.params.id;
+
+  try {
+    const post = await Post.findByIdAndUpdate(id, req.body, {
+      useFindAndModify: false
+    });
+    if (!post) {
+      return res.status(404).send({
+        message: `Cannot update post with id ${id}, maybe it was not found.`
+      });
+    } else {
+      return res.status(200).send({
+        message: 'Post updated successfully.',
+        post
+      });
+    }
+  } catch (err: any) {
+    return res.status(500).send({
+      message: `Error updating post with id ${id}.`,
+      error: err.message
+    });
+  }
+};
+
+// remove post by id
+export const deletePost = async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  try {
+    const post = await Post.findByIdAndRemove(id);
+    if (!post) {
+      return res.status(404).send({
+        message: `Cannot delete post with id ${id}, maybe it was not found.`
+      });
+    } else {
+      return res.status(200).send({
+        message: 'Post deleted successfully.'
+      });
+    }
+  } catch (err: any) {
+    return res.status(500).send({
+      message: `Error deleting post with id ${id}.`,
+      error: err.message
+    });
+  }
+};
---
+++ server/app/routes/post.routes.ts
@@ -0,0 +1,42 @@
+// routes for posts
+import * as controller from '../controllers/post.controller';
+import { Request, Response, NextFunction, Application, Router } from 'express';
+import middlewares from '../middlewares';
+
+const verifyToken = middlewares.authJWT.verifyToken;
+
+const postRoutes = (app: Application, router: Router) => {
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    res.header(
+      'Access-Control-Allow-Headers',
+      'x-access-token, Origin, Content-Type, Accept'
+    );
+    next();
+  });
+
+  // create new post
+  router.post('/', [verifyToken], controller.createPost);
+
+  // get post by id
+  router.get('/:id', [verifyToken], controller.findOnePost);
+
+  // get post by id with comments
+  router.get('/:id/comments', [verifyToken], controller.getPostWithComments)
+
+  // get all posts by user id
+  router.get(
+    '/user/:id',
+    [verifyToken],
+    controller.findPostsByUserId
+  );
+
+  // update post by id
+  router.put('/:id', [verifyToken], controller.updatePost);
+
+  // delete post by id
+  router.delete('/:id', [verifyToken], controller.deletePost);
+
+  app.use('/api/post', router);
+};
+
+export default postRoutes;
