6cefebed760915bf1e5332002c7c638d9457b2b7
Karin <karin.stenwall@gmail.com>
2021-10-06 17:06:01 +0200

    feat: mui form in login and regiser

--- client/pages/login/index.tsx
+++ client/pages/login/index.tsx
@@ -1,28 +1,46 @@
 import type { NextPageWithLayout } from 'next';
-import type { ReactNode } from 'react'
+import type { ReactNode } from 'react';
 import StartLayout from '../../components/layout/StartLayout';
 import Link from 'next/link';
+import { Box, Button, TextField, Typography } from '@mui/material';
 
 const Login: NextPageWithLayout = () => {
   return (
     <>
-      <h1>Login</h1>
+      <div className="form-wrapper">
+        <Typography variant="h4" component="h2">
+          Login
+        </Typography>
 
-      <ul>
-        <li>
-          <Link href="/feed">
-            <a>Login (will take you to feed)</a>
-          </Link>
-        </li>
-      </ul>
+        <Box component="form">
+          <TextField id="email-login" label="Email" type="email" />
+          <TextField id="pwd-login" label="Password" type="password" />
+          {/* <Button variant="contained" type="submit" disableElevation>
+          Login
+        </Button> */}
+          <Button
+            href="/feed"
+            variant="contained"
+            type="submit"
+            disableElevation
+          >
+            Login
+          </Button>
+          <Typography variant="subtitle1" component="span" gutterBottom>
+            Don't have an account?{' '}
+            <Link href="/login">
+              <a>Register.</a>
+            </Link>
+          </Typography>
+        </Box>
+        <span>(will take you to feed)</span>
+      </div>
     </>
   );
 };
 
 Login.getLayout = (page: ReactNode) => {
-  return (
-    <StartLayout>{page}</StartLayout>
-  )
-}
+  return <StartLayout>{page}</StartLayout>;
+};
 
 export default Login;
--- client/pages/register/index.tsx
+++ client/pages/register/index.tsx
@@ -1,28 +1,62 @@
 import type { NextPageWithLayout } from 'next';
-import type { ReactNode } from 'react'
+import type { ReactNode } from 'react';
 import StartLayout from '../../components/layout/StartLayout';
 import Link from 'next/link';
+import { Box, Button, TextField, Typography } from '@mui/material';
 
 const Register: NextPageWithLayout = () => {
   return (
     <>
-      <h1>Register</h1>
+      <div className="form-wrapper">
+        <Typography variant="h4" component="h2">
+          Register
+        </Typography>
 
-      <ul>
-        <li>
-          <Link href="/feed">
-            <a>Register (will take you to feed)</a>
-          </Link>
-        </li>
-      </ul>
+        <Box component="form">
+          <TextField id="firstname" label="Firstname" type="text" required />
+          <TextField id="lastname" label="Lastname" type="text" required />
+          <TextField id="username" label="Username" type="text" required />
+          <TextField
+            id="desc"
+            label="Description"
+            placeholder="Describe yourself to your neighbours!"
+            rows={4}
+            multiline
+          />
+          <TextField id="email" label="Email" type="email" required />
+          <TextField id="pwd" label="Password" type="password" required />
+          <TextField
+            id="pwd-confirm"
+            label="Confirm password"
+            type="password"
+            required
+          />
+          {/* <Button variant="contained" type="submit" disableElevation>
+          Register
+        </Button> */}
+          <Button
+            href="/feed"
+            variant="contained"
+            type="submit"
+            disableElevation
+          >
+            Register
+          </Button>
+          <Typography variant="subtitle1" component="span" gutterBottom>
+            Already have an account?{' '}
+            <Link href="/login">
+              <a>Login.</a>
+            </Link>
+          </Typography>
+        </Box>
+        <span>(will take you to feed)</span>
+      </div>
     </>
   );
 };
 
 Register.getLayout = (page: ReactNode) => {
-  return (
-    <StartLayout>{page}</StartLayout>
-  )
-}
+  return <StartLayout>{page}</StartLayout>;
+};
 
 export default Register;
