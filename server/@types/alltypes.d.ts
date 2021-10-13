b9640f2ed874831f39caf1aa2244dbf1ac38c46f
Karin <karin.stenwall@gmail.com>
2021-10-13 23:13:19 +0200

    fix: adminbro build problem w typed dependencies

---
+++ server/@types/alltypes.d.ts
@@ -0,0 +1,6 @@
+declare module '@admin-bro/mongoose';
+declare module 'mocha';
+declare module 'react-redux';
+declare module 'styled-components';
+declare module 'styled-system';
+declare module 'flat';
