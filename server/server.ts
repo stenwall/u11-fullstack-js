15c407b8ab299fb7ce5ecaaff710162c2db756b9
Karin <karin.stenwall@gmail.com>
2021-10-13 14:44:45 +0200

    wip: set upp adminbro panel for superadmins

--- server/server.ts
+++ server/server.ts
@@ -6,11 +6,20 @@ import cors from 'cors';
 import models from './app/models';
 import routes from './app/routes';
 import cookieParser from 'cookie-parser';
+import AdminBro, { AdminBroOptions } from 'admin-bro';
+import AdminBroMongoose from '@admin-bro/mongoose';
+import AdminBroExpress from '@admin-bro/express';
+import * as bcrypt from 'bcryptjs';
 
 const app = express();
 const House = models.House;
+const User = models.User;
+const Post = models.Post;
+const Comment = models.Comment;
 
 mongoose.Promise = global.Promise;
+
+AdminBro.registerAdapter(AdminBroMongoose);
 
 const corsOptions: object = {
   origin: config.CLIENT_URL,
@@ -22,12 +31,6 @@ const options: object = {
   useUnifiedTopology: true
 };
 
-app.use(cookieParser());
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
 mongoose
   .connect(config.DB_URI, options)
   .then(() => {
@@ -38,6 +41,32 @@ mongoose
     console.log('Cannot connect to the database.', err);
     process.exit();
   });
+
+const superAdmin = new AdminBro({
+  resources: [User, House, Post, Comment],
+  rootPath: '/admin'
+});
+
+const adminRouter = AdminBroExpress.buildAuthenticatedRouter(superAdmin, {
+  authenticate: async (email, password) => {
+    const user = await User.findOne({ email });
+    if (user) {
+      const matched = await bcrypt.compare(password, user.password);
+      if (matched) {
+        return user;
+      }
+    }
+    return false;
+  },
+  cookiePassword: config.SUPER_COOKIE_PWD
+});
+
+app.use(superAdmin.options.rootPath, adminRouter);
+app.use(cookieParser());
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const initial = () => {
   House.estimatedDocumentCount((err: any, count: number) => {
@@ -54,8 +83,7 @@ const initial = () => {
         if (err) {
           console.log('error', err);
         }
-
-        console.log('added `Höstvetet` to house collection');
+        console.log('added "Höstvetet" to house collection');
       });
     }
   });
@@ -64,8 +92,8 @@ const initial = () => {
 app.set('view engine', 'pug');
 
 app.get('/', (req: express.Request, res: express.Response) => {
-  res.render('index')
-})
+  res.render('index');
+});
 
 app.listen(config.PORT, () => {
   console.log(`Server is running on port: ${config.PORT}.`);
