a0bd66c4f091605a872b22b5af594f5bf3807e6d
Karin <karin.stenwall@gmail.com>
2021-10-14 00:55:06 +0200

    chore: fix env-files in config frontend

--- client/helpers/http-common.ts
+++ client/helpers/http-common.ts
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
 // new axios instance with defaults
 export default axios.create({
-  baseURL: API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     'content-type': 'application/x-www-form-urlencoded',
   },
